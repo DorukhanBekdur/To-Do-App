713b8294783ae5770cfa52dd12ece61f38333d1e
Dorukhan Bekdur <dorukhanbekdur@gmail.com>
2025-04-01 01:07:39 +0300

    Input Box Bug Fixed

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -19,7 +19,7 @@ function TodoForm({ addTodo }) {
         value={text}
         onChange={(e) => setText(e.target.value)}
         placeholder="Yeni görev ekle..."
-        className="w-full px-4 py-2 rounded-xl bg-white/20 backdrop-blur-sm text-white placeholder:text-gray-300 border border-white/20 focus:outline-none focus:ring-2 focus:ring-blue-800 shadow-inner transition"
+        className="w-full px-4 py-2 rounded-xl bg-white/20 backdrop-blur-sm text-black placeholder:text-gray-300 border border-white/20 focus:outline-none focus:ring-2 focus:ring-blue-800 shadow-inner transition"
       />
 
       <button
